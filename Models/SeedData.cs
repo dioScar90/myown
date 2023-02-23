d3f41068cf0b72d4e5ccb7efd7c0dad1e387817f
Diogo L Scarmagnani <diogols@live.com>
2023-02-23 11:57:55 -0300

    serpentsToAdd

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -10,6 +10,189 @@ public static class SeedData
 {
     public static void Initialize(IServiceProvider serviceProvider)
     {
+        Serpent[] serpentsToAdd = new Serpent[]
+        {
+            new()
+            {
+                PopularName = "Surucucu",
+                CientificName = "Lachesis muta",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Cascavel",
+                CientificName = "Crotalus durissus",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Jararaca-da-mata",
+                CientificName = "Bothrops jararaca",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Jararaca-do-norte",
+                CientificName = "Bothrops atrox",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Jararaca-da-seca",
+                CientificName = "Bothrops erythromelas",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Jararaca-ilhoa",
+                CientificName = "Bothrops insularis",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Jararacuçu",
+                CientificName = "Bothrops jararacussu",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Urutu-cruzeiro",
+                CientificName = "Bothrops alternatus",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Cobra-coral (Caatinga)",
+                CientificName = "Micrurus ibiboboca",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Cobra-coral (Mata Atlântica)",
+                CientificName = "Micrurus corallinus",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Cobra-coral (Amazônia)",
+                CientificName = "Micrurus albicintus",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Jiboia",
+                CientificName = "Boa constrictor",
+                FamilyType = Family.Boidae
+            },
+            new()
+            {
+                PopularName = "Jiboia-arco-íris",
+                CientificName = "Epicrates assisi",
+                FamilyType = Family.Boidae
+            },
+            new()
+            {
+                PopularName = "Sucuri-verde",
+                CientificName = "Eunectes murinus",
+                FamilyType = Family.Boidae
+            },
+            new()
+            {
+                PopularName = "Sucuri-amarela",
+                CientificName = "Eunectes notaeus",
+                FamilyType = Family.Boidae
+            },
+            new()
+            {
+                PopularName = "Píton-reticulada",
+                CientificName = "Malayopython reticulatus",
+                FamilyType = Family.Pythonidae
+            },
+            new()
+            {
+                PopularName = "Cobra-rei",
+                CientificName = "Ophiophagus hannah",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Taipan-do-interior",
+                CientificName = "Oxyuranus microlepidotus",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Mamba-negra",
+                CientificName = "Dendroaspis polylepis",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Mamba-verde-oriental",
+                CientificName = "Dendroaspis angusticeps",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Naja-cuspideira",
+                CientificName = "Hemachatus haemachatus",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Naja-egípcia",
+                CientificName = "Naja haje",
+                FamilyType = Family.Elapidae
+            },
+            new()
+            {
+                PopularName = "Víbora-do-gabão",
+                CientificName = "Bitis gabonica",
+                FamilyType = Family.Viperidae
+            },
+            new()
+            {
+                PopularName = "Periquitamboia",
+                CientificName = "Corallus caninus",
+                FamilyType = Family.Boidae
+            },
+            new()
+            {
+                PopularName = "Cobra-verde",
+                CientificName = "Liophis typhlus",
+                FamilyType = Family.Dipsadidae
+            },
+            new()
+            {
+                PopularName = "Muçurana",
+                CientificName = "Clelia clelia",
+                FamilyType = Family.Dipsadidae
+            },
+            new()
+            {
+                PopularName = "Caninana",
+                CientificName = "Spilotes pullatus",
+                FamilyType = Family.Colubridae
+            },
+            new()
+            {
+                PopularName = "Cobra-cipó-marrom",
+                CientificName = "Chironius quadricarinatus",
+                FamilyType = Family.Colubridae
+            },
+            new()
+            {
+                PopularName = "Jararacuçu-do-brejo",
+                CientificName = "Palusophis bifossatus",
+                FamilyType = Family.Colubridae
+            },
+            new()
+            {
+                PopularName = "Titanoboa",
+                CientificName = "Titanoboa cerrejonensis",
+                FamilyType = Family.Boidae
+            }
+        };
         using (var context = new SerpentContext(
             serviceProvider.GetRequiredService<
                 DbContextOptions<SerpentContext>>()))
@@ -17,6 +200,12 @@ public static class SeedData
             // Look for any movies.
             if (context.Serpent.Any())
             {
+                Console.WriteLine("\n\n\nAdicionou\n\n\n");
+                List<Serpent> tempList = context.Serpent.ToList();
+                List<Serpent> toAdd = serpentsToAdd.Where(s => !tempList.Any(tl => tl.CientificName == s.CientificName)).ToList();
+                context.Serpent.AddRange(toAdd);
+                context.SaveChanges();
+                Console.WriteLine("\n\n\nAdicionou MESMO!\n\n\n");
                 return;   // DB has been seeded
             }
             context.Serpent.AddRange(
